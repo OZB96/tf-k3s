5969f0851a58a51630245cfa99bbd936650a78fa
Omar Bazaid <omar.vip3@gmail.com>
2020-08-30 14:31:53 +0300

    add private output

--- lab/outputs.tf
+++ lab/outputs.tf
@@ -1,3 +1,11 @@
 output "control_plane" {
   value = aws_instance.controlplane[0].public_ip
 }
+
+output "worker1" {
+  value = aws_instance.worker[0].private_ip
+}
+
+output "worker2" {
+  value = aws_instance.worker[1].private_ip
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "control_plane_ip" {
   value = module.dev.control_plane
 }
+
+output "worker1_ip" {
+  value = module.dev.worker1
+}
+
+output "worker2_ip" {
+  value = module.dev.worker2
+}
