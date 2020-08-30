1a49317799d8af442a38cf80b1c4c460be85e2b4
Omar Bazaid <omar.vip3@gmail.com>
2020-08-30 14:03:54 +0300

    fix port

--- lab/main.tf
+++ lab/main.tf
@@ -158,7 +158,7 @@ resource "aws_security_group" "controlplane" {
   ingress {
     from_port       = 22
     to_port         = 22
-    protocol        = "-1"
+    protocol        = "tcp"
     cidr_blocks     =["0.0.0.0/0"]
   }
 
