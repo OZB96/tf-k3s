954e6a98394bf5933accb35cd9aa3035f6be6b82
Omar Bazaid <omar.vip3@gmail.com>
2020-08-30 13:30:43 +0300

    fix ingress port

--- lab/main.tf
+++ lab/main.tf
@@ -149,12 +149,19 @@ resource "aws_security_group" "controlplane" {
 //And i change here
 
   ingress {
-    from_port       = 0
-    to_port         = 0
+    from_port       = 6443
+    to_port         = 6443
+    protocol        = "tcp"
+    cidr_blocks     =["0.0.0.0/0"]
+  }
+ 
+  ingress {
+    from_port       = 22
+    to_port         = 22
     protocol        = "-1"
     cidr_blocks     =["0.0.0.0/0"]
-   // security_groups = [aws_security_group.bastion.id]
   }
+
 
 }
 
