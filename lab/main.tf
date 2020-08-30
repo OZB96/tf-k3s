6718325bd1bf10df66e5cf28934a0f2b1b00b2c3
Omar Bazaid <omar.vip3@gmail.com>
2020-08-31 01:34:36 +0300

    fix provissioner

--- lab/main.tf
+++ lab/main.tf
@@ -283,8 +283,8 @@ resource "aws_instance" "worker" {
 
   inline = [ 
   "export K3S_HOST=controlplane.phi.com",
-  "export K3S_TOKEN="$(nc.traditional $K3S_HOST 12345)"",
-  "export K3S_URL=https://$K3S_HOST:6443",
+  "export K3S_TOKEN="$$(nc.traditional $$K3S_HOST 12345)"",
+  "export K3S_URL=https://$$K3S_HOST:6443",
   "env",
   "curl -sfL https://get.k3s.io | sh -",
   ]
