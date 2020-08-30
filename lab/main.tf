d00b5a04e5f237e5564c0e368d11003cc1cd4620
Omar Bazaid <omar.vip3@gmail.com>
2020-08-30 22:48:08 +0300

    fix secrity groups

--- lab/main.tf
+++ lab/main.tf
@@ -191,21 +191,6 @@ resource "random_id" "keypair" {
 resource "aws_security_group" "worker" {
   vpc_id = aws_vpc.lab.id
   tags   = module.tags_worker.tags
-  
-  ingress {
-    from_port       = 30000
-    to_port         = 32767
-    protocol        = "-1"
-    cidr_blocks     =["0.0.0.0/0"]
-    security_groups = [aws_security_group.controlplane.id]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     =["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group_rule" "egress_to_all" {
