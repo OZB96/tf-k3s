71611eac182f4eb83ab396a0474fef4984f61133
Omar Bazaid <omar.vip3@gmail.com>
2020-09-02 02:33:09 +0300

    s

--- lab/main.tf
+++ lab/main.tf
@@ -227,7 +227,7 @@ resource "aws_route53_record" "controlplane" {
   name    = "controlplane"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.controlplane.0.public_ip]
+  records = [aws_instance.controlplane.0.private_ip]
 }
 
 resource "aws_instance" "controlplane" {
@@ -266,7 +266,7 @@ resource "aws_instance" "worker" {
   inline = [ 
   "sleep 30",
   "export K3S_HOST=controlplane.phi.com",
-  "export K3S_TOKEN=$(nc.traditional $K3S_HOST 12345)",
+  "export K3S_TOKEN=$(nc ${aws_instance.controlplane.0.private_ip} 12345)",
   "export K3S_URL=https://$K3S_HOST:6443",
   "env",
   "curl -sfL https://get.k3s.io | sh -",
