b7ba964eecf4d7e438cab556360afe178bfb2bbf
Omar Bazaid <omar.vip3@gmail.com>
2020-09-02 00:38:48 +0300

    add provisoner

--- lab/main.tf
+++ lab/main.tf
@@ -236,25 +236,6 @@ resource "aws_instance" "controlplane" {
   subnet_id              = aws_subnet.controlplane[count.index].id
   vpc_security_group_ids = [aws_security_group.controlplane.id]
   tags                   = module.tags_controlplane.tags
- 
-  provisioner "remote-exec" {
-
-  inline = [ 
-  "sudo systemctl restart print_token",
-  ]
-
-  connection {
-  type = "ssh"
-  user = "ubuntu"
-  host = self.public_ip
-  private_key = file("./ssh/id_rsa")
-  //bastion_host = aws_instance.controlplane.0.public_ip
-  //bastion_private_key = file("./ssh/id_rsa")
-  //bastion_user = "ubuntu"
-  }
-  }  
-
-
   //added
    key_name               = aws_key_pair.lab_keypair.id
 }
@@ -291,7 +272,7 @@ resource "aws_instance" "worker" {
     aws_instance.controlplane
   ]
 }
-
+/*
 resource "null_resource" "cluster" {
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
@@ -315,4 +296,4 @@ resource "null_resource" "cluster" {
     aws_instance.worker
   ]
 }
-
+*/
